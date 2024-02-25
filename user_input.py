6c0222357c008f17dfb3c855f7a837a92e46f85b
IvanKimani <issakimani8@gmail.com>
2024-02-25 23:49:49 +0300

    First Commit

---
+++ user_input.py
@@ -0,0 +1,12 @@
+# request for user name..store in var called name
+
+name = input("What is your name?")
+
+#ask for age
+age = input("How old are you?")
+
+#ask for location
+location = input("What is your residence?")
+
+# print output of name, age,location
+print(f"Hello {name}, you are {age} years old and reside in {location}.")
